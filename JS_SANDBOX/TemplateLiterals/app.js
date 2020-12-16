ad3f82f5685995ee92493bbd20e0ca21efef7fab
Jacob Cavazos <cavazos.jacob.r@heb.com>
2020-12-16 09:16:13 -0600

    lesson update

---
+++ JS_SANDBOX/TemplateLiterals/app.js
@@ -0,0 +1,30 @@
+const name = 'John';
+const age = 31;
+const job = 'Web Developer';
+const city = 'San Antonio';
+let html;
+
+// Without template string (es5)
+html = '<ul><li>Name: ' + name + '</li><li>Age: ' + age + ' </li><li>Job: ' + job + '</li><li>City: ' + city + '</li></ul>';
+
+function hello() {
+  return 'hello';
+}
+// With template strings/literals (es6)
+html = `
+<ul>
+<li>Name: ${name}</li>
+<li>Age: ${age}</li>
+<li>Job: ${job}</li>
+<li>City: ${city}</li>
+<li>${2 + 2}</li>
+<li>${hello()}</li>
+<li>${age > 30 ? 'Over 30' : 'Under 30'}</li>
+</ul>
+`;
+  
+  
+  
+  
+  
+document.body.innerHTML = html;
